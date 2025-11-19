25f2f55aaa822aaa675e62a4b538ffcbb782a470
omidshayan <omidshayan.dev@gmail.com>
2025-11-19 23:57:12 +0430

    change session dashboard

--- Http/Controllers/dashboard/Dashboard.php
+++ Http/Controllers/dashboard/Dashboard.php
@@ -11,10 +11,9 @@ class Dashboard extends App
     public function index()
     {
         $this->db = DataBase::getInstance();
-        if (isset($_SESSION['af_em_id'])) {
-            $user = $this->db->select('SELECT * FROM `employees` WHERE id = ?', [$_SESSION['af_em_id']])->fetch();
+        if (isset($_SESSION['hsm_em_id'])) {
+            $user = $this->db->select('SELECT * FROM `employees` WHERE id = ?', [$_SESSION['hsm_em_id']])->fetch();
         }
         require_once(BASE_PATH . '/resources/views/app/dashboard/index.php');
-
     }
 }
