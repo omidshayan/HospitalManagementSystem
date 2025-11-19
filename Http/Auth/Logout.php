db8e9be954701686175906abcf6f3bd0b3b98c54
omidshayan <omidshayan.dev@gmail.com>
2025-11-19 23:56:11 +0430

    logout

--- Http/Auth/Logout.php
+++ Http/Auth/Logout.php
@@ -10,27 +10,24 @@ class Logout extends Auth
     public function logout()
     {
         $db = DataBase::getInstance();
-        if (isset($_SESSION['er_em_id'])) {
+        if (isset($_SESSION['hsm_em_id'])) {
             // به‌روزرسانی وضعیت توکن در دیتابیس
-            $db->update('employees', $_SESSION['er_em_id'], ['expire_remember_token', 'remember_token'], [0, null]);
-            unset($_SESSION['er_em_id']);
-            unset($_SESSION['er_em_name']);
-            unset($_SESSION['er_em_image']);
+            $db->update('employees', $_SESSION['hsm_em_id'], ['expire_remember_token', 'remember_token'], [0, null]);
+            unset($_SESSION['hsm_em_id']);
+            unset($_SESSION['hsm_em_name']);
+            unset($_SESSION['hsm_em_image']);
             unset($_SESSION['permissions']);
             unset($_SESSION['admin']);
 
             session_destroy();
 
-            // حذف کوکی‌ها
-            $expiry = time() - 3600; // یک ساعت پیش
-            setcookie("er_em_co", '', $expiry, '/', '', true, true); // حذف کوکی
+            $expiry = time() - 3600;
+            setcookie("hsm_user", '', $expiry, '/', '', true, true);
 
-            // هدایت به صفحه لاگین
             $this->redirect('login');
             exit();
         }
 
-        // در صورت عدم وجود سشن، هدایت به صفحه لاگین
         $this->redirect('login');
         exit();
     }
