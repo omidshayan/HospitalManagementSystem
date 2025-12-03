9d7ed41a9267a476f8fb803f289be4aa906c2934
omidshayan <omidshayan.dev@gmail.com>
2025-12-03 13:09:54 +0430

    end form

--- resources/views/app/prescriptions/add-prescription.php
+++ resources/views/app/prescriptions/add-prescription.php
@@ -48,47 +48,26 @@
                         <div class="one">
                             <div class="label-form mb5 fs14"> زمان مصرف </div>
                             <select name="frequency" required>
+                                <option value="4">بعد از غذا</option>
+                                <option value="4">قبل از غذا</option>
                                 <option value="1">هر 1 ساعت</option>
                                 <option value="1">هر 2 ساعت</option>
                                 <option value="1">هر 3 ساعت</option>
                                 <option value="1">هر 4 ساعت</option>
                                 <option value="2">دو بار در روز</option>
                                 <option value="1">یک بار در روز</option>
-                                <option value="2">دو بار در روز</option>
-                                <option value="3">سه بار در روز</option>
                                 <option value="4">چهار بار در روز</option>
                             </select>
                         </div>
+                    </div>
+                    <div class="inputs d-flex">
                         <div class="one">
-                            <div class="label-form mb5 fs14" for="name">مقدار مصرف در هر نوبت</div>
+                            <div class="label-form mb5 fs14" for="name">مقدار / واحد مصرف در هر نوبت</div>
                             <select name="frequency" required>
                                 <option value="1">یک قاشق</option>
                                 <option value="1">دو قاشق</option>
                             </select>
                         </div>
-                    </div>
-                    <div class="inputs d-flex">
-
-                        <div class="one">
-                            <div class="label-form mb5 fs14" for="name">زمان مصرف نسبت به غذا</div>
-                            <select name="food_relation">
-                                <option value="before">قبل از غذا</option>
-                                <option value="after">بعد از غذا</option>
-                                <option value="with">همزمان با غذا</option>
-                                <option value="none">بدون توجه به غذا</option>
-                            </select>
-                        </div>
-                        <div class="one">
-                            <div class="label-form mb5 fs14">مدت زمان مصرف </div>
-                            <select name="duration_unit">
-                                <option value="day">دو روز</option>
-                                <option value="week">سه روز</option>
-                                <option value="month">چهار روز</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="inputs d-flex">
-
                         <div class="one">
                             <div class="label-form mb5 fs14">توضیحات اضافی</div>
                             <textarea name="description" placeholder="توضیحات را وارد نمایید"></textarea>
