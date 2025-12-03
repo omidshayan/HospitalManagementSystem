00e567c92db8a659868163251868e066b87048bd
omidshayan <omidshayan.dev@gmail.com>
2025-12-03 23:45:34 +0430

    add confirm js

--- resources/views/app/prescriptions/add-prescription.php
+++ resources/views/app/prescriptions/add-prescription.php
@@ -103,7 +103,7 @@
                     <div class="mr30 bold">
                         نسخه بیمار: <span><?= ($prescription['patient_name']) ? $prescription['patient_name'] : 'ثبت نشده' ?></span>
                         <?php
-                            if(empty($drugList)){ ?>
+                        if (empty($drugList)) { ?>
                             <a href="<?= url('delete-sale-invoice/' . $prescription['id']) ?>" class="color-red text-underline">حذف نسخه</a>
                         <?php }
                         ?>
@@ -146,7 +146,7 @@
                                 </td>
 
                                 <td>
-                                    <a href="<?= url('delete-prescription-list/' . $item['id']) ?>" class="delete-product flex-justify-align">
+                                    <a href="<?= url('delete-prescription-list/' . $item['id']) ?>" class="delete-drug flex-justify-align">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 448 512">
                                             <path fill="#ff0000" d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z" />
                                         </svg>
@@ -166,8 +166,41 @@
             </div>
         <?php }
         ?>
-
-
     </div>
+
+    <!-- confirm for delete -->
+    <script>
+        $(document).ready(function() {
+
+            document.querySelectorAll(".delete-drug").forEach(function(element) {
+                element.addEventListener("click", function(event) {
+                    let confirmDelete = confirm("آیا از حذف دارو اطمینان دارید؟");
+                    if (!confirmDelete) {
+                        event.preventDefault();
+                    }
+                });
+            });
+
+            document.querySelectorAll(".delete-prescription").forEach(function(element) {
+                element.addEventListener("click", function(event) {
+                    let confirmDelete = confirm("آیا از حذف نسخه اطمینان دارید؟");
+                    if (!confirmDelete) {
+                        event.preventDefault();
+                    }
+                });
+            });
+
+            document.querySelectorAll(".delete-all-products").forEach(function(element) {
+                element.addEventListener("click", function(event) {
+                    let confirmDelete = confirm("آیا از حذف همه فاکتورها اطمینان دارید؟");
+                    if (!confirmDelete) {
+                        event.preventDefault();
+                    }
+                });
+            });
+
+        });
+    </script>
+
 
     <?php include_once('resources/views/layouts/footer.php') ?>
