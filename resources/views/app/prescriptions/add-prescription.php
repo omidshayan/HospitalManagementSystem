47b9a8f089edced2f51a51ec0bfecd6119ef62c5
omidshayan <omidshayan.dev@gmail.com>
2025-12-03 22:50:58 +0430

    add table item list

--- resources/views/app/prescriptions/add-prescription.php
+++ resources/views/app/prescriptions/add-prescription.php
@@ -9,11 +9,8 @@
     <!-- Start content -->
     <div class="content">
         <div class="content-title">ثبت نسخه جدید</div>
-        <!-- start page content -->
-
 
         <form action="<?= url('drug-prescription-store') ?>" method="POST">
-
             <!-- select details drug -->
             <div class="box-container">
                 <div class="insert">
@@ -94,11 +91,80 @@
 
                     <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?>" />
                     <input type="submit" id="submit" value="ثبت" class="btn" />
+                </div>
+            </div>
         </form>
+
+        <!-- prescription items -->
+        <?php
+        if ($sale_invoice) { ?>
+            <div class="content-container mb30">
+                <div class="mb10 fs14 d-flex"> اقلام فاکتور شماره: <?= $sale_invoice['invoice_number'] ?>
+                    <div class="mr30 bold"> مشتری: <span><?= ($seller) ? $seller['user_name'] : 'عمومی' ?></span></div>
+                    <div class="mr30 bold">
+                        <span><?= isset($total_debt['debtor']) ? 'حساب قبلی: ' . number_format($total_debt['debtor']) : '' ?></span>
+                    </div>
+                </div>
+                <table class="fl-table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>نام محصول</th>
+                            <th>تعداد بسته</th>
+                            <th>تعداد عدد</th>
+                            <th>تعداد کل</th>
+                            <th>قیمت فروش واحد</th>
+                            <th>قیمت کل</th>
+                            <th>ویرایش</th>
+                            <th>حذف</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $number = 1;
+                        foreach ($cart_lists as $item) {
+                            $unitPrices = $this->calculateUnitPrices($item);
+                        ?>
+                            <tr>
+                                <td class="color-orange"><?= $number ?></td>
+                                <td><?= $item['product_name'] ?></td>
+                                <td><?= $this->formatNumber($item['package_qty']) ?? 0 ?></td>
+                                <td><?= $this->formatNumber($item['unit_qty']) ?? 0 ?></td>
+                                <td><?= $this->formatNumber($item['quantity']) ?? 0 ?></td>
+                                <td><?= $this->formatNumber($unitPrices['sell'], 2) ?></td>
+                                <td><?= $this->formatNumber($item['item_total_price']) ?></td>
+                                <td>
+                                    <a href="<?= url('edit-sale-product-cart/' . $item['id']) ?>" class="color-orange flex-justify-align">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" class="color-orange" />
+                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                                        </svg>
+                                    </a>
+                                </td>
+
+                                <td>
+                                    <a href="<?= url('delete-sale-product-cart/' . $item['id']) ?>" class="delete-product flex-justify-align">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 448 512">
+                                            <path fill="#ff0000" d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z" />
+                                        </svg>
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php
+                            $number++;
+                        }
+                        ?>
+                    </tbody>
+                    <tbody></tbody>
+                </table>
+                <div class="flex-justify-align mt20 paginate-section">
+                    <div class="table-info fs12">تعداد کل: <?= count($cart_lists) ?></div>
+                </div>
+            </div>
+        <?php }
+        ?>
+
+
     </div>
-    </div>
-    <!-- end page content -->
-    </div>
-    <!-- End content -->
 
     <?php include_once('resources/views/layouts/footer.php') ?>
