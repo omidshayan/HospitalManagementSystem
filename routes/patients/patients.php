476a60fa8b6e66ca1793b3a68b1b405546cffd94
omidshayan <omidshayan.dev@gmail.com>
2025-12-05 20:39:19 +0430

    fixed route

--- routes/patients/patients.php
+++ routes/patients/patients.php
@@ -1,14 +1,17 @@
 <?php
-require_once 'Http/Controllers/employees/Employee.php';
+require_once 'Http/Controllers/patients/Patient.php';
 
-// add product routes
-uri('add-employee', 'App\Employee', 'addEmployee');
-uri('employees', 'App\Employee', 'showEmployees');
-uri('employee-store', 'App\Employee', 'employeeStore', 'POST');
-uri('employee-details/{id}', 'App\Employee', 'employeeDetails');
-uri('edit-employee/{id}', 'App\Employee', 'editEmployee');
-uri('edit-employee/store/{id}', 'App\Employee', 'editEmployeeStore', 'POST');
-uri('change-status-employee/{id}', 'App\Employee', 'changeStatus');
+// add patients routes
+uri('patients', 'App\Patient', 'patients');
+
+
+
+// uri('employees', 'App\Employee', 'showEmployees');
+// uri('employee-store', 'App\Employee', 'employeeStore', 'POST');
+// uri('employee-details/{id}', 'App\Employee', 'employeeDetails');
+// uri('edit-employee/{id}', 'App\Employee', 'editEmployee');
+// uri('edit-employee/store/{id}', 'App\Employee', 'editEmployeeStore', 'POST');
+// uri('change-status-employee/{id}', 'App\Employee', 'changeStatus');
 
 
 
