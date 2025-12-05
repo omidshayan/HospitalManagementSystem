d12f90f6a67448b5607b0111950505897b9ba6a7
omidshayan <omidshayan.dev@gmail.com>
2025-12-06 01:07:56 +0430

    add base

--- resources/views/app/prints/prescription.php
+++ resources/views/app/prints/prescription.php
@@ -1,5 +1,54 @@
-
 <!-- invoice print -->
-<div class="form-container" id="print">
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+.a4{
+    width: 297mm;
+    height: 210mm;
+    background-color: green;
+}
+    .main-print {
+        width: 940px;
+        height: 620px;
+        display: flex;
+        justify-content: space-between;
+        background-color: black;
+    }
+
+    .print-right {
+        width: 170px;
+        background-color: red;
+        height: 600px;
+    }
+        .print-left {
+        width: 170px;
+        background-color: red;
+        height: 600px;
+    }
+    .print-center{
+        margin-top: 30px;
+        color: white;
+    }
+</style>
+
+
+<div class="a4">
+
+    <div class="form-container" id="print">
+        <div class="main-print">
+            <div class="print-left">
+
+            </div>
+            <div class="print-center">
+                <h1>کلینیک مهر سلامت</h1>
+            </div>
+
+                        <div class="print-right">
+                اسم مریض:............
+            </div>
+        </div>
+    </div>
 
 </div>
