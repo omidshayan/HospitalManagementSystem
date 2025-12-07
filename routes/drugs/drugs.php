fab8ed93add204abd0cf9933770c3169eb67e6ba
omidshayan <omidshayan.dev@gmail.com>
2025-12-07 23:55:51 +0430

    end all route drugs

--- routes/drugs/drugs.php
+++ routes/drugs/drugs.php
@@ -5,13 +5,13 @@ require_once 'Http/Controllers/drugs/Drug.php';
 uri('add-drug', 'App\Drug', 'addDrug');
 uri('drug-store', 'App\Drug', 'drugStore', 'POST');
 uri('drugs', 'App\Drug', 'showDrugs');
+uri('drug-details/{id}', 'App\Drug', 'drugDetails');
+uri('edit-drug/{id}', 'App\Drug', 'editDrug');
+uri('edit-drug/store/{id}', 'App\Drug', 'editDrugStore', 'POST');
+uri('change-status-drug/{id}', 'App\Drug', 'changeDrugStatus');
 
 
 
-uri('employee-details/{id}', 'App\Employee', 'employeeDetails');
-uri('edit-employee/{id}', 'App\Employee', 'editEmployee');
-uri('edit-employee/store/{id}', 'App\Employee', 'editEmployeeStore', 'POST');
-uri('change-status-employee/{id}', 'App\Employee', 'changeStatus');
 
 
 
