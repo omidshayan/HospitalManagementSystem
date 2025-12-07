b7a8057b64adcbec46473038b3cef7a3da113c41
omidshayan <omidshayan.dev@gmail.com>
2025-12-08 00:00:09 +0430

    start view edit drug

--- resources/views/app/drugs/edit-drug.php
+++ resources/views/app/drugs/edit-drug.php
@@ -1,6 +1,6 @@
     <!-- start sidebar -->
     <?php
-    $title = 'ویرایش کارمند: ' . $employee['employee_name'];
+    $title = 'ویرایش دارو: ' . $drug['name'];
     include_once('resources/views/layouts/header.php');
     include_once('public/alerts/check-inputs.php');
     include_once('public/alerts/toastr.php');
@@ -10,20 +10,20 @@
 
     <!-- Start content -->
     <div class="content">
-        <div class="content-title">ویرایش کارمند: <?= $employee['employee_name'] ?></div>
+        <div class="content-title">ویرایش دارو: <?= $drug['name'] ?></div>
         <br />
         <!-- start page content -->
         <div class="box-container">
             <div class="insert">
-                <form action="<?= url('edit-employee/store/' . $employee['id']) ?>" method="POST" enctype="multipart/form-data">
+                <form action="<?= url('edit-employee/store/' . $drug['id']) ?>" method="POST" enctype="multipart/form-data">
                     <div class="inputs d-flex">
                         <div class="one">
                             <div class="label-form mb5 fs14">نام و تخلص <?= _star ?> </div>
-                            <input type="text" class="checkInput" name="employee_name" placeholder="نام و تخلص را وارد نمایید" maxlength="40" value="<?= $employee['employee_name'] ?>" />
+                            <input type="text" class="checkInput" name="name" placeholder="نام و تخلص را وارد نمایید" maxlength="40" value="<?= $drug['name'] ?>" />
                         </div>
                         <div class="one">
                             <div class="label-form mb5 fs14">شماره <?= _star ?> </div>
-                            <input type="number" class="checkInput" name="phone" placeholder="شماره را وارد نمایید" value="<?= $employee['phone'] ?>" />
+                            <input type="number" class="checkInput" name="phone" placeholder="شماره را وارد نمایید" value="<?= $drug['phone'] ?>" />
                         </div>
                     </div>
                     <div class="inputs d-flex">
@@ -34,7 +34,7 @@
                         </div>
                         <div class="one">
                             <div class="label-form mb5 fs14">مقدار معاش</div>
-                            <input type="number" value="<?= $employee['salary_price'] ?>" name="salary_price" placeholder="مقدار معاش را وارد نمایید" />
+                            <input type="number" value="<?= $drug['salary_price'] ?>" name="salary_price" placeholder="مقدار معاش را وارد نمایید" />
                         </div>
                     </div>
                     <div class="inputs d-flex">
@@ -43,7 +43,7 @@
                             <div class="label-form mb5 fs14" for="name">وظیفه</div>
                             <select name="position" id="mySelect" class="checkSelect">
                                 <?php foreach ($positions as $position) : ?>
-                                    <option value="<?= $position['name'] ?>" <?= $position['name'] == $employee['position'] ? 'selected' : '' ?>>
+                                    <option value="<?= $position['name'] ?>" <?= $position['name'] == $drug['position'] ? 'selected' : '' ?>>
                                         <?= $position['name'] ?>
                                     </option>
                                 <?php endforeach; ?>
@@ -51,7 +51,7 @@
                         </div>
                         <div class="one">
                             <div class="label-form mb5 fs14">توضیحات</div>
-                            <textarea name="description" placeholder="توضیحات را وارد نمایید"><?= $employee['description'] ?></textarea>
+                            <textarea name="description" placeholder="توضیحات را وارد نمایید"><?= $drug['description'] ?></textarea>
                         </div>
                     </div>
 
@@ -66,7 +66,7 @@
                     </div>
 
                     <div>
-                        <img src="<?= ($employee['image'] ? asset('public/images/employees/' . $employee['image']) : asset('public/assets/img/empty.png')) ?>" onclick="openModal('<?php echo asset('public/images/employees/' . $employee['image']); ?>')"
+                        <img src="<?= ($drug['image'] ? asset('public/images/drugس/' . $drug['image']) : asset('public/assets/img/empty.png')) ?>" onclick="openModal('<?php echo asset('public/images/drugس/' . $drug['image']); ?>')"
                             class="img cursor-p" alt="logo">
                     </div>
                     <div class="fs11">تصویر فعلی</div>
