56328ae4aa193ad706b90283138d3c116c9f3c03
omidshayan <omidshayan.dev@gmail.com>
2025-12-10 01:12:06 +0430

    add perm in edit empoyee form

--- resources/views/app/employees/edit-employee.php
+++ resources/views/app/employees/edit-employee.php
@@ -71,6 +71,121 @@
                     </div>
                     <div class="fs11">تصویر فعلی</div>
 
+
+                    <div class="accordion-title color-orange">تعیین سطح دسترسی</div>
+                    <div class="accordion-content">
+                        <div class="child-accordioin">
+
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="prescriptionPrint">
+                                    <span> چاپ خودکار نسخه‌ها</span>
+                                    <input type="checkbox" name="prescriptionPrint" id="prescriptionPrint" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="showPatients">
+                                    <span> نمایش مریضان</span>
+                                    <input type="checkbox" name="showPatients" id="showPatients" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="addPrescription">
+                                    <span> ثبت نسخه</span>
+                                    <input type="checkbox" name="addPrescription" id="addPrescription" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="showPrescription">
+                                    <span> نمایش نسخه‌ها</span>
+                                    <input type="checkbox" name="showPrescription" id="showPrescription" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="addEmployee">
+                                    <span> ثبت کارمند جدید</span>
+                                    <input type="checkbox" name="addEmployee" id="addEmployee" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="showEmployees">
+                                    <span> نمایش کارمندان</span>
+                                    <input type="checkbox" name="showEmployees" id="showEmployees" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="positions">
+                                    <span> مدیریت وظایف کارمندان</span>
+                                    <input type="checkbox" name="positions" id="positions" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="addDrug">
+                                    <span> ثبت دارو</span>
+                                    <input type="checkbox" name="addDrug" id="addDrug" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="showDrugs">
+                                    <span> نمایش داروها</span>
+                                    <input type="checkbox" name="showDrugs" id="showDrugs" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="catDrug">
+                                    <span> مدیریت دسته بندی‌های دارو</span>
+                                    <input type="checkbox" name="catDrug" id="catDrug" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="unitDrug">
+                                    <span> مدیریت واحد شمارش</span>
+                                    <input type="checkbox" name="unitDrug" id="unitDrug" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="numberDrugs">
+                                    <span> تنظیمات تعداد دارو</span>
+                                    <input type="checkbox" name="numberDrugs" id="numberDrugs" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="intakeTime">
+                                    <span> تنظیمات زمان مصرف</span>
+                                    <input type="checkbox" name="intakeTime" id="intakeTime" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="dosage">
+                                    <span> تنظیمات مقدار مصرف</span>
+                                    <input type="checkbox" name="dosage" id="dosage" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="intakeInstructions">
+                                    <span> تنظیمات طریقه مصرف</span>
+                                    <input type="checkbox" name="intakeInstructions" id="intakeInstructions" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+                            <div class="detailes-culomn d-flex cursor-p h30 align-center">
+                                <label class="title-detaile d-flex justify-between w100 h30 align-center cursor-p" for="settingPrescription">
+                                    <span> تنظیمات نسخه</span>
+                                    <input type="checkbox" name="settingPrescription" id="settingPrescription" class="w20 p0 m0 h22">
+                                </label>
+                            </div>
+
+                        </div>
+                    </div>
+
+
                     <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token'] ?>" />
                     <input type="submit" id="submit" value="ویــرایــش" class="btn bold" />
                 </form>
