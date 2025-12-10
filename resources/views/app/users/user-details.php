b75c04e8a83afd6d5c037d7d8da70b0085006d27
omidshayan <omidshayan.dev@gmail.com>
2025-12-10 12:27:18 +0430

    open modal img

--- resources/views/app/users/user-details.php
+++ resources/views/app/users/user-details.php
@@ -1,8 +1,9 @@
 <!-- start sidebar -->
 <?php
-$title = 'جزئیات کاربر: ' . $user['name'];
+$title = 'جزئیات مریض: ' . $user['user_name'];
 include_once('resources/views/layouts/header.php');
 include_once('resources/views/scripts/change-status.php');
+include_once('resources/views/scripts/show-img-modal.php');
 ?>
 <!-- end sidebar -->
 <div id="alert" class="alert" style="display: none;">حالم بده، با برنامه نویس مه تماس بگیر :(</div>
@@ -17,7 +18,7 @@ include_once('resources/views/scripts/change-status.php');
 <div class="content">
 
     <div class="content-title d-flex justify-between">
-        <span class="" id="openModalBtn"> <span class="content-title"> جزئیات کارمند : <?= $user['name'] ?></span>
+        <span class="" id="openModalBtn"> <span class="content-title"> جزئیات مریض : <?= $user['user_name'] ?></span>
         </span>
     </div>
 
@@ -29,7 +30,7 @@ include_once('resources/views/scripts/change-status.php');
             <div class="child-accordioin">
                 <div class="detailes-culomn d-flex cursor-p">
                     <div class="title-detaile">نام</div>
-                    <div class="info-detaile"><?= $user['name'] ?></div>
+                    <div class="info-detaile"><?= $user['user_name'] ?></div>
                 </div>
                 <div class="detailes-culomn d-flex cursor-p">
                     <div class="title-detaile">نام پدر</div>
@@ -53,14 +54,16 @@ include_once('resources/views/scripts/change-status.php');
                 </div>
                 <div class="detailes-culomn d-flex align-center cursor-p">
                     <div class="title-detaile">عکس</div>
-                    <div class="info-detaile">
-                        <?= ($user['image'] ? '<a href="' . asset('public/images/users/' . $user['image']) . '" target="_blank"><img src="' . asset('public/images/users/' . $user['image']) . '" class="w50" alt=""></a>' : '- - - - ') ?>
+                    <div class="info-detaile d-flex align-center">
+                        <?= $user['image']
+                            ? '<img class="w50 cursor-p" src="' . asset('public/images/users/' . $user['image']) . '" alt="logo" onclick="openModal(\'' . asset('public/images/users/' . $user['image']) . '\')">'
+                            : ' - - - - ' ?>
                     </div>
                 </div>
                 <div class="detailes-culomn d-flex align-center cursor-p">
                     <div class="title-detaile"><a href="#" data-url="<?= url('change-status-user') ?>" data-id="<?= $user['id'] ?>" class="changeStatus color btn p5 w100 m10 center" id="submit">تغییر وضعیت</a></div>
                     <div class="info-detaile">
-                        <div class="w100 m10 center status status-column" id="status"><?= ($user['state'] == 1) ? '<span class="color-green">فعال</span>' : '<span class="color-red">غیرفعال</span>' ?></div>
+                        <div class="w100 m10 center status status-column" id="status"><?= ($user['status'] == 1) ? '<span class="color-green">فعال</span>' : '<span class="color-red">غیرفعال</span>' ?></div>
                     </div>
                 </div>
             </div>
