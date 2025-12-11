ba4c5ce9bd519c340fa27a5825bbf980e370ce3c
omidshayan <omidshayan.dev@gmail.com>
2025-12-11 22:20:06 +0430

    clean print class

--- Http/Controllers/prints/Prints.php
+++ Http/Controllers/prints/Prints.php
@@ -4,28 +4,6 @@ namespace App;
 
 class Prints extends App
 {
-    // management of years page
-    // public function prescriptionPrint($id)
-    // {
-    //     $this->middleware(true, true, 'general', true);
-
-    //     $sale_invoice_print = $this->db->select(
-    //         'SELECT 
-    //         si.*,
-    //         u.user_name,
-    //         u.address,
-    //         u.phone,
-    //         ca.balance AS account_balance
-    //      FROM invoices si
-    //      LEFT JOIN users u ON u.id = si.user_id
-    //      LEFT JOIN account_balances ca ON ca.user_id = si.user_id AND ca.branch_id = si.branch_id
-    //      WHERE si.id = ?',
-    //         [$id]
-    //     )->fetch();
-
-    //     require_once(BASE_PATH . '/resources/views/app/sales/add-sale.php');
-    // }
-
     // show presctiption for print
     public function print()
     {
@@ -131,24 +109,4 @@ class Prints extends App
 
         require_once(BASE_PATH . '/resources/views/app/prints/prescriptionPrint.php');
     }
-
-
-
-    // print invoice
-    // public function autoPrint()
-    // {
-    //     dd('tuo');
-    //     $this->middleware(true, true, 'general', true);
-
-    //     $prescription = $this->db->select('SELECT * FROM prescriptions WHERE `status` = ?', [2])->fetch();
-
-    //     if (!$prescription) {
-    //         require_once BASE_PATH . '/404.php';
-    //         exit();
-    //     }
-
-    //     $factor_infos = $this->db->select('SELECT * FROM prescription_settings')->fetch();
-
-    //     require_once(BASE_PATH . '/resources/views/app/prints/prescription.php');
-    // }
 }
