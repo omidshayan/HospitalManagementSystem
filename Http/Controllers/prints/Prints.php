2174b7164b4e2d447b5348b7707444370487016b
omidshayan <omidshayan.dev@gmail.com>
2025-12-12 01:34:29 +0430

    comment print

--- Http/Controllers/prints/Prints.php
+++ Http/Controllers/prints/Prints.php
@@ -9,31 +9,31 @@ class Prints extends App
     {
         $this->middleware(true, true, 'prescriptionPrint', true);
 
-        $prescription = $this->db->select(
-            'SELECT p.*, 
-                e.employee_name,
-                e.expertise
-        FROM prescriptions p
-        JOIN employees e ON e.id = p.doctor_id
-        WHERE p.status = ?
-        ORDER BY p.id ASC
-        LIMIT 1',
-            [2]
-        )->fetch();
+        // $prescription = $this->db->select(
+        //     'SELECT p.*, 
+        //         e.employee_name,
+        //         e.expertise
+        // FROM prescriptions p
+        // JOIN employees e ON e.id = p.doctor_id
+        // WHERE p.status = ?
+        // ORDER BY p.id ASC
+        // LIMIT 1',
+        //     [2]
+        // )->fetch();
 
-        $items = [];
+        // $items = [];
 
-        if ($prescription) {
-            $this->db->update('prescriptions', $prescription['id'], ['status'], [3]);
+        // if ($prescription) {
+        //     $this->db->update('prescriptions', $prescription['id'], ['status'], [3]);
 
-            $items = $this->db->select(
-                'SELECT *
-             FROM prescription_items
-             WHERE prescription_id = ?
-             ORDER BY id ASC',
-                [$prescription['id']]
-            )->fetchAll();
-        }
+        //     $items = $this->db->select(
+        //         'SELECT *
+        //      FROM prescription_items
+        //      WHERE prescription_id = ?
+        //      ORDER BY id ASC',
+        //         [$prescription['id']]
+        //     )->fetchAll();
+        // }
 
         $prescriptions = $this->db->select(
             'SELECT p.*, e.employee_name
