116af9f3b0264051fd139e793db16d25515e15b9
omidshayan <omidshayan.dev@gmail.com>
2025-12-14 02:52:34 +0430

    add new table

--- resources/views/app/prescriptions/add-prescription.php
+++ resources/views/app/prescriptions/add-prescription.php
@@ -222,7 +222,7 @@
             <div class="content-container mb30 mt20">
                 <div class="mb10 fs14 d-flex justify-between">
                     <div class="mr30">
-                        <span><?= (!empty($drugList) || !empty($recommended)) ? 'لیست دواها | آزمایشات' : 'لیست خالی است' ?></span>
+                        <span><?= (!empty($drugList)) ? 'لیست دواها' : '' ?></span>
                         <?php
                         if (empty($drugList) && empty($recommended)) { ?>
 
@@ -234,56 +234,91 @@
                         <?= $drugList ? '<a href="' . url('close-prescription-store/' . $prescription['id']) . '" class="color btn p5-20 bg-success bold pa close-p" id="closePrescriptionBtn">ثبت نسخه</a>' : '' ?>
                     </div>
                 </div>
-                <table class="fl-table">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>نام دارو</th>
-                            <th>تعداد</th>
-                            <th>زمان مصرف</th>
-                            <th>مقدار | واحد</th>
-                            <th>طریقه مصرف</th>
-                            <th>توضیحات</th>
-                            <!-- <th>ویرایش</th> -->
-                            <th>حذف</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $number = 1;
-                        foreach ($drugList as $item) {
-                        ?>
-                            <tr>
-                                <td class="color-orange"><?= $number ?></td>
-                                <td><?= $item['drug_name'] ?></td>
-                                <td><?= $item['drug_count'] ?></td>
-                                <td><?= ($item['interval_time']) ?: '- - - -' ?></td>
-                                <td><?= ($item['dosage']) ?: '- - - -' ?></td>
-                                <td><?= ($item['usage_instruction']) ?: '- - - -' ?></td>
-                                <td><?= $item['description'] ?: '- - - -' ?></td>
-                                <!-- <td>
-                                    <a href="<?= url('edit-prescription-list/' . $item['id']) ?>" class="color-orange flex-justify-align">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" class="color-orange" />
-                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
-                                        </svg>
-                                    </a>
-                                </td> -->
 
-                                <td>
-                                    <a href="<?= url('delete-prescription-list/' . $item['id']) ?>" class="delete-drug flex-justify-align">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 448 512">
-                                            <path fill="#ff0000" d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z" />
-                                        </svg>
-                                    </a>
-                                </td>
+                <?php
+                if (!empty($drugList)) { ?>
+                    <table class="fl-table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>نام دارو</th>
+                                <th>تعداد</th>
+                                <th>زمان مصرف</th>
+                                <th>مقدار | واحد</th>
+                                <th>طریقه مصرف</th>
+                                <th>توضیحات</th>
+                                <th>حذف</th>
                             </tr>
-                        <?php
-                            $number++;
-                        }
-                        ?>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $number = 1;
+                            foreach ($drugList as $item) {
+                            ?>
+                                <tr>
+                                    <td class="color-orange"><?= $number ?></td>
+                                    <td><?= $item['drug_name'] ?></td>
+                                    <td><?= $item['drug_count'] ?></td>
+                                    <td><?= ($item['interval_time']) ?: '- - - -' ?></td>
+                                    <td><?= ($item['dosage']) ?: '- - - -' ?></td>
+                                    <td><?= ($item['usage_instruction']) ?: '- - - -' ?></td>
+                                    <td><?= $item['description'] ?: '- - - -' ?></td>
+                                    <td>
+                                        <a href="<?= url('delete-prescription-list/' . $item['id']) ?>" class="delete-drug flex-justify-align">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 448 512">
+                                                <path fill="#ff0000" d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z" />
+                                            </svg>
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php
+                                $number++;
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                <?php }
+                ?>
+
+
+                <?php
+                if (!empty($tests)) { ?>
+
+                    <div class="p5">لیست آزمایشات</div>
+                    <table class="fl-table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>نام آزمایش</th>
+                                <th>حذف</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <?php
+                            $number = 1;
+                            foreach ($tests as $item) {
+                            ?>
+                                <tr>
+                                    <td class="color-orange"><?= $number ?></td>
+                                    <td><?= $item['test_name'] ?></td>
+                                    <td>
+                                        <a href="<?= url('delete-prescription-list/' . $item['id']) ?>" class="delete-drug flex-justify-align">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 448 512">
+                                                <path fill="#ff0000" d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z" />
+                                            </svg>
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php
+                                $number++;
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                <?php }
+                ?>
+
                 <div class="flex-justify-align mt20 paginate-section">
                     <div class="table-info fs12">تعداد کل: <?= count($drugList) ?></div>
                 </div>
