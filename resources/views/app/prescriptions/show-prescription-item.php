d3f52c51a7675cb4871e8236144327fd6b67dc26
omidshayan <omidshayan.dev@gmail.com>
2025-12-13 23:02:08 +0430

    add class for item

--- resources/views/app/prescriptions/show-prescription-item.php
+++ resources/views/app/prescriptions/show-prescription-item.php
@@ -39,7 +39,7 @@ include_once('public/alerts/toastr.php');
                 <div class="body-pre-print">
 
                     <!-- drugs list -->
-                    <div class="p-drugs-print d-flex">
+                    <div class="p-drugs-print d-flex flex-c bg-green-opacity pr">
                         <table>
                             <thead>
                                 <tr class="fs12 p-color-title">
@@ -68,7 +68,7 @@ include_once('public/alerts/toastr.php');
                                 ?>
                             </tbody>
                         </table>
-                        <div>dsfas sadf</div>
+                        <div class="pa">dsfas sadf</div>
                     </div>
 
                     <!-- left infos -->
