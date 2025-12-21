4147aff0da5864b320e8547954b7dbf0cef803b1
omidshayan <omidshayan.dev@gmail.com>
2025-12-22 00:57:00 +0430

    uncomment

--- resources/views/layouts/header.php
+++ resources/views/layouts/header.php
@@ -59,7 +59,7 @@
         </div>
       </div>
       <div class="sidebar-item">
-        
+
         <ul>
 
           <!-- dashboard -->
@@ -149,7 +149,7 @@
           <?php endif; ?>
 
           <!-- employees -->
-          <!-- <?php if ($this->hasAccess('parentEmployee')): ?>
+          <?php if ($this->hasAccess('parentEmployee')): ?>
             <li class="sidebar-menu ri-dashboard-line sidebar-menu-item has-dropdown">
               <a href="#" class="d-flex align-center justify-between dddd">
                 <div>
@@ -182,7 +182,7 @@
                 <?php endif; ?>
               </ul>
             </li>
-          <?php endif; ?> -->
+          <?php endif; ?>
 
           <!-- Drug -->
           <?php if ($this->hasAccess('parentDrug')): ?>
@@ -276,7 +276,7 @@
           <?php endif; ?>
 
           <!-- settings -->
-          <!-- <?php if ($this->hasAccess('parentDrug')): ?>
+          <?php if ($this->hasAccess('parentDrug')): ?>
             <li class="sidebar-menu ri-dashboard-line sidebar-menu-item has-dropdown">
               <a href="#" class="d-flex align-center justify-between dddd">
                 <div>
@@ -297,7 +297,7 @@
                 <?php endif; ?>
               </ul>
             </li>
-          <?php endif; ?> -->
+          <?php endif; ?>
 
           <!-- profile -->
           <li class="sidebar-menu">
