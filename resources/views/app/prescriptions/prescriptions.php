aa984af48ddb633e0c816f7a87e2212b7883d9bd
omidshayan <omidshayan.dev@gmail.com>
2025-12-22 01:40:57 +0430

    clean

--- resources/views/app/prescriptions/prescriptions.php
+++ resources/views/app/prescriptions/prescriptions.php
@@ -44,7 +44,7 @@ include_once('public/alerts/toastr.php');
                         <td><?= $item['employee_name'] ?></td>
                         <td><?= $item['patient_name'] ?? '- - - -' ?></td>
                         <td><?= jdate('Y/m/d', strtotime($item['created_at'])) ?></td>
-                        <td><a href="<?=url('print-item/' . $item['id'])?>" target="_blank" class="text-underline color">چاپ</a></td>
+                        <td><a href="<?= url('print-item/' . $item['id']) ?>" target="_blank" class="text-underline color">چاپ</a></td>
 
                         <td>
                             <a href="<?= url('edit-prescription/' . $item['id']) ?>" class="color-orange">
