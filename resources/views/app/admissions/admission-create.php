b0f1c36f3f1321c46ce0695268b37f53d336bd21
omidshayan <omidshayan.dev@gmail.com>
2025-12-24 22:02:28 +0430

    add js for age

--- resources/views/app/admissions/admission-create.php
+++ resources/views/app/admissions/admission-create.php
@@ -84,4 +84,41 @@
         });
     </script>
 
+    <!-- age -->
+    <script>
+        // generate age
+        function toEnglishDigits(str) {
+            return str.replace(/[۰-۹]/g, d => "۰۱۲۳۴۵۶۷۸۹".indexOf(d));
+        }
+
+        function getCurrentPersianYear() {
+            let today = new Date();
+            let formatter = new Intl.DateTimeFormat('fa-AF', {
+                year: 'numeric'
+            });
+            let persianYear = formatter.format(today);
+
+            return parseInt(toEnglishDigits(persianYear));
+        }
+
+        document.getElementById('ageInput').addEventListener('input', function() {
+            let age = parseInt(this.value);
+            let birthYearTag = document.getElementById('birthYear');
+            let birthYearInput = document.getElementById('birthYearInput');
+
+            if (!age || age <= 0) {
+                birthYearTag.textContent = '';
+                birthYearInput.value = '';
+                return;
+            }
+
+            let currentPersianYear = getCurrentPersianYear();
+            let birthYear = currentPersianYear - age;
+
+            birthYearTag.textContent = birthYear;
+
+            birthYearInput.value = birthYear;
+        });
+    </script>
+
     <?php include_once('resources/views/layouts/footer.php') ?>
