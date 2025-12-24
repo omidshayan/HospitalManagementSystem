ce0f96681b1b488d42f7edf7ec16b4384f050f68
omidshayan <omidshayan.dev@gmail.com>
2025-12-25 03:26:14 +0430

    change name sidebar

--- resources/views/layouts/header.php
+++ resources/views/layouts/header.php
@@ -336,7 +336,7 @@
           <?php endif; ?>
 
           <!-- settings -->
-          <!-- <?php if ($this->hasAccess('parentDrug')): ?>
+          <?php if ($this->hasAccess('parentDrug')): ?>
             <li class="sidebar-menu ri-dashboard-line sidebar-menu-item has-dropdown">
               <a href="#" class="d-flex align-center justify-between dddd">
                 <div>
@@ -352,12 +352,12 @@
               <ul class="sidebar-dropdown-menu">
                 <?php if ($this->hasAccess('addDrug')): ?>
                   <a href="<?= url('pre-print-settings') ?>">
-                    <li class="sidebar-dropdown-menu-item">تنظیمات چاپ و نسخه</li>
+                    <li class="sidebar-dropdown-menu-item">تنظیمات عمومی</li>
                   </a>
                 <?php endif; ?>
               </ul>
             </li>
-          <?php endif; ?> -->
+          <?php endif; ?>
 
           <!-- profile -->
           <li class="sidebar-menu">
@@ -366,7 +366,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w17" viewBox="0 0 16 16">
                   <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
                 </svg>
-                <span class="mr5">تنظیمات حساب</span>
+                <span class="mr5">پروفایل</span>
               </span>
             </a>
           </li>
