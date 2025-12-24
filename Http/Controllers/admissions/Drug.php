9df0489c87b05a7d8a53011d58c415896d10fe0b
omidshayan <omidshayan.dev@gmail.com>
2025-12-24 17:53:22 +0430

    end route and create class

---
+++ Http/Controllers/admissions/Drug.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App;
+
+class Drug extends App
+{
+    // drugs
+    public function addDrug()
+    {
+        $this->middleware(true, true, 'general', true);
+        $drugCategories = $this->db->select('SELECT * FROM drug_categories WHERE `status` = ?', [1])->fetchAll();
+        $units = $this->db->select('SELECT * FROM units WHERE `status` = ?', [1])->fetchAll();
+        require_once(BASE_PATH . '/resources/views/app/drugs/add-drug.php');
+    }
+
+    // store employee
+    public function drugStore($request)
+    {
+        $this->middleware(true, true, 'addDrug', true, $request, true);
+
+        // check empty form
+        if ($request['name'] == '' || $request['category_id'] == '' || $request['unit'] == '') {
+            $this->flashMessage('error', _emptyInputs);
+        }
+
+        $existingDrug = $this->db->select('SELECT * FROM drugs WHERE `name` = ?', [$request['name']])->fetch();
+        if ($existingDrug) {
+            $this->flashMessage('error', _repeat);
+        } else {
+
+            $request = $this->validateInputs($request, ['image' => false]);
+
+            // check image
+            $this->handleImageUpload($request['image'], 'images/drugs');
+
+            $this->db->insert('drugs', array_keys($request), $request);
+            $this->flashMessage('success', _success);
+        }
+    }
+
+    // show drugs
+    public function showDrugs()
+    {
+        $this->middleware(true, true, 'showDrugs');
+        $drugs = $this->db->select('SELECT * FROM drugs ORDER BY id DESC')->fetchAll();
+        require_once(BASE_PATH . '/resources/views/app/drugs/show-drugs.php');
+        exit();
+    }
+
+    // edit drug page
+    public function editDrug($id)
+    {
+        $this->middleware(true, true, 'general', true);
+
+        $drug = $this->db->select('SELECT * FROM drugs WHERE id = ?', [$id])->fetch();
+        $drugCategories = $this->db->select('SELECT * FROM drug_categories WHERE `status` = ?', [1])->fetchAll();
+        $units = $this->db->select('SELECT * FROM units WHERE `status` = ?', [1])->fetchAll();
+        if ($drug != null) {
+            require_once(BASE_PATH . '/resources/views/app/drugs/edit-drug.php');
+            exit();
+        } else {
+            require_once(BASE_PATH . '/404.php');
+            exit();
+        }
+    }
+
+    // edit employee store
+    public function editDrugStore($request, $id)
+    {
+        $this->middleware(true, true, 'general', true, $request, true);
+
+        // check empty form
+        if ($request['name'] == '' || $request['category_id'] == '' || $request['unit'] == '') {
+            $this->flashMessage('error', _emptyInputs);
+        }
+
+        $existDrug = $this->db->select(
+            'SELECT id FROM drugs WHERE name = ? AND id != ? LIMIT 1',
+            [$request['name'], $id]
+        )->fetch();
+
+        if ($existDrug) {
+            $this->flashMessage('error', 'نام وارد شده قبلاً ثبت شده است.');
+            return;
+        }
+
+        // check upload photo
+        $this->updateImageUpload($request, 'image', 'drugs', 'drugs', $id);
+
+        $this->db->update('drugs', $id, array_keys($request), $request);
+        $this->flashMessageTo('success', _success, url('drugs'));
+    }
+
+    // drug detiles page
+    public function drugDetails($id)
+    {
+        $this->middleware(true, true, 'general');
+
+        $drug = $this->db->select("
+            SELECT 
+                d.*, 
+                c.cat_name,
+                u.unit_name
+            FROM drugs d
+            LEFT JOIN drug_categories c ON d.category_id = c.id
+            LEFT JOIN units u ON d.unit = u.id
+            WHERE d.id = ?
+            LIMIT 1
+        ", [$id])->fetch();
+
+        if ($drug) {
+            require_once(BASE_PATH . '/resources/views/app/drugs/drug-details.php');
+            exit();
+        } else {
+            require_once(BASE_PATH . '/404.php');
+            exit();
+        }
+    }
+
+    // change status drug
+    public function changeDrugStatus($id)
+    {
+        $this->middleware(true, true, 'general');
+
+        $drug = $this->db
+            ->select('SELECT * FROM drugs WHERE id = ?', [$id])
+            ->fetch();
+
+        if (!$drug) {
+            require_once BASE_PATH . '/404.php';
+            exit;
+        }
+
+        $newState = ($drug['status'] == 1) ? 2 : 1;
+
+        $this->db->update('drugs', $drug['id'], ['status'], [$newState]);
+        $this->send_json_response(true, _success, $newState);
+    }
+}
--- routes/admissions/admissions.php
+++ routes/admissions/admissions.php
@@ -2,13 +2,13 @@
 require_once 'Http/Controllers/admissions/Admission.php';
 
 // admissions routes
-uri('admission/create', 'App\Drug', 'admissionCreate');
-uri('admission/store', 'App\Drug', 'drugStore', 'POST');
-uri('admissions', 'App\Drug', 'showDrugs');
-uri('admission/details/{id}', 'App\Drug', 'drugDetails');
-uri('edit/admission/{id}', 'App\Drug', 'editDrug');
-uri('edit/admission/store/{id}', 'App\Drug', 'editDrugStore', 'POST');
-uri('change/status/admission/{id}', 'App\Drug', 'changeDrugStatus');
+uri('admission/create', 'App\Admission', 'admissionCreate');
+uri('admission/store', 'App\Admission', 'admissionStore', 'POST');
+uri('admissions', 'App\Admission', 'admissions');
+uri('admission/details/{id}', 'App\Admission', 'admissionDetails');
+uri('edit/admission/{id}', 'App\Admission', 'editAdmission');
+uri('edit/admission/store/{id}', 'App\Admission', 'editAdmissionStore', 'POST');
+uri('change/status/admission/{id}', 'App\Admission', 'changeAdmissionStatus');
 
 
 
