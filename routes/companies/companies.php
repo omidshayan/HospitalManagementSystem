e101c7d05df99ba0291e2f693f8589844e5b51ee
omidshayan <omidshayan.dev@gmail.com>
2025-12-26 22:30:33 +0430

    end routes

--- routes/companies/companies.php
+++ routes/companies/companies.php
@@ -1,10 +1,10 @@
 <?php
-require_once 'Http/Controllers/dosage/Dosage.php';
+require_once 'Http/Controllers/companies/Company.php';
 
-// dosage routes
-uri('dosage', 'App\Dosage', 'dosage');
-uri('dosage-store', 'App\Dosage', 'dosageStore', 'POST');
-uri('edit-dosage/{id}', 'App\Dosage', 'editDosage');
-uri('edit-dosage-store/{id}', 'App\Dosage', 'editDosageStore', 'POST');
-uri('dosage-details/{id}', 'App\Dosage', 'dosageDetails');
-uri('change-status-dosage/{id}', 'App\Dosage', 'changeStatusDosage');
+// company routes
+uri('companies', 'App\Company', 'companies');
+uri('company-store', 'App\Company', 'companyStore', 'POST');
+uri('edit-company/{id}', 'App\Company', 'editCompany');
+uri('edit-company-store/{id}', 'App\Company', 'editCompanyStore', 'POST');
+uri('company-details/{id}', 'App\Company', 'companyDetails');
+uri('change-status-company/{id}', 'App\Company', 'changeStatusCompany');
