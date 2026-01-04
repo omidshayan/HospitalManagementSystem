056bc61bf22acffe2da2ed8b9484adef0dd31a90
omidshayan <omidshayan.dev@gmail.com>
2026-01-04 16:00:32 +0430

    check exist core

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 
-require __DIR__ . '/core/sys/map.php';
+$core_path = __DIR__ . '/core/sys/';
+$required_files = ['env.php', 'map.php', 'stat.php'];
 
-echo m_x();
+if (!is_dir(__DIR__ . '/core')) {
+    die("Critical system folder missing!");
+}
 
-
+foreach ($required_files as $file) {
+    if (!file_exists($core_path . $file)) {
+        die("Critical system file missing: $file");
+    }
+}
 
 require_once 'config.php';
 header('location: login');
