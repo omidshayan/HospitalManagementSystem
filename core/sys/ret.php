f64dce96394ec5b5126843b652cd94b5e79c24c7
omidshayan <omidshayan.dev@gmail.com>
2026-01-04 18:28:55 +0430

    reostor and satart license

--- core/sys/ret.php
+++
@@ -1,6 +0,0 @@
-<?php
-// core/sys/secret.php
-function secret_key() {
-    // یک رشته پیچیده و محرمانه انتخاب کن
-    return 'XyZ!@#SecretKey2026_$%^&*()';
-}
--- index.php
+++ index.php
@@ -1,31 +1,4 @@
 <?php
 
-
-// NOTE این قسمت باید چند بخش دیگه تکرار بشه تا امینت داشته باشه
-require_once __DIR__ . '/core/sys/license_check.php';
-require_once __DIR__ . '/core/sys/map.php';
-
-$licenseFile = __DIR__ . '/license.lic';
-
-if (!file_exists($licenseFile)) {
-    die("License file not found!");
-}
-
-$licenseJson = file_get_contents($licenseFile);
-$currentFingerprint = m_x();
-
-$result = verify_license($licenseJson, $currentFingerprint);
-
-if (!$result['valid']) {
-    die("License is invalid: " . $result['message']);
-}
-
-if ($result['days_left'] <= 30) {
-    echo "<div style='color:orange; font-weight:bold;'>Warning: Your license will expire in {$result['days_left']} day(s).</div>";
-}
-
-// ادامه اجرای برنامه ...
-
-
 require_once 'config.php';
 header('location: login');
