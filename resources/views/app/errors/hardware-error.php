90645df215e2d8c44bab38a0bcc371d1fcb63db5
omidshayan <omidshayan.dev@gmail.com>
2026-01-06 14:49:10 +0430

    end hardware-error page

--- resources/views/app/errors/hardware-error.php
+++ resources/views/app/errors/hardware-error.php
@@ -1,5 +1,5 @@
 <?php
-$title = 'محدودیت دسترسی سیستم'; ?>
+$title = 'محدودیت دسترسی کامپیوتر'; ?>
 <style>
 	* {
 		padding: 0;
@@ -13,8 +13,22 @@ $title = 'محدودیت دسترسی سیستم'; ?>
 		width: 200px;
 	}
 	.box-container {
-		background-color: #171722ff;
 		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.cont{
+		background-color: #171722ff;
+		width: 500px;
+		height: 300px;
+		text-align: center;
+		border-radius: 20px;
+		color: whitesmoke;
+		border: 1px solid #7e7e7e85;
+	}
+	.err-title{
+		margin: 50px 0 20px 0;
 	}
 </style>
 <div class="box-container">
@@ -23,7 +37,9 @@ $title = 'محدودیت دسترسی سیستم'; ?>
 		<div class="center">
 			<img src="<?= asset('public/assets/img/warning.svg') ?>" class="w200" alt="warning">
 		</div>
-
+		<h2 class="err-title">این کامپیوتر مجاز به استفاده از برنامه نمی‌باشد</h2>
+		<div>لطفاً با پشتیبانی تماس بگیرید</div>
+		<div>0799192027</div>
 	</div>
 </div>
 </div>
