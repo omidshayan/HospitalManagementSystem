327b5ac0f8f47c8ecf459010711a00f912ce125c
omidshayan <omidshayan.dev@gmail.com>
2026-01-06 14:41:13 +0430

    add cont

--- resources/views/app/errors/hardware-error.php
+++ resources/views/app/errors/hardware-error.php
@@ -1,28 +1,30 @@
 <?php
 $title = 'محدودیت دسترسی سیستم'; ?>
-<div class="content">
-	<div class="box-container">
+<style>
+	* {
+		padding: 0;
+		margin: 0;
+	}
+	body {
+		background-color: #222334;
+		height: 100vh !important;
+	}
+	.w200 {
+		width: 200px;
+	}
+	.box-container {
+		background-color: #171722ff;
+		height: 100vh;
+	}
+</style>
+<div class="box-container">
+	<div class="cont">
+
 		<div class="center">
 			<img src="<?= asset('public/assets/img/warning.svg') ?>" class="w200" alt="warning">
 		</div>
-		<div class="main-content app-content mt-0">
-			<div class="side-app">
-				<div class="main-container center">
-					<!-- PAGE-HEADER -->
-					<div class="page-header">
-						<h1 class="page-title a-mt10 color-red">عدم دسترسی</h1>
-					</div>
-					<!-- PAGE-HEADER END -->
 
-					<!-- main content start -->
-					<div class="box-content-container mb-100 color-red mt15">
-						شما به این قسمت دسترسی ندارید!
-					</div>
-					<!-- main content end -->
-				</div>
-			</div>
-		</div>
 	</div>
 </div>
+</div>
 <!-- content end -->
-<?php require_once(BASE_PATH . '/resources/views/layouts/footer.php') ?>
