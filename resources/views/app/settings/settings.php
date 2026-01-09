410d7a96bae2b346728f6cacc0f38b592c574d25
omidshayan <omidshayan.dev@gmail.com>
2026-01-09 23:25:33 +0430

    change check

--- resources/views/app/settings/settings.php
+++ resources/views/app/settings/settings.php
@@ -69,12 +69,12 @@ include_once('resources/views/scripts/activeNotActive.php');
         <div class="fs14 color-green">تنظیمات عمومی</div>
         <hr class="hr">
 
-        <!-- print in doctor panel -->
-        <div class="allow-invoice d-flex justify-between mt15">
+        <!-- drug type doctor panel -->
+        <div class="allow-invoice d-flex justify-between mt20">
             <span>
-                چاپ نسخه جدید در پنل داکتر
-                <span id="sellStatus" class="status-text <?= ($settings['single_print'] == 1) ? 'color-green' : 'color-orange' ?>">
-                    (<?= ($settings['single_print'] == 1) ? 'فعال' : 'غیر فعال' ?>)
+                نمایش نوع دارو
+                <span id="intake_time" class="status-text <?= ($settings['intake_time'] == 1) ? 'color-green' : 'color-orange' ?>">
+                    (<?= ($settings['intake_time'] == 1) ? 'فعال' : 'غیر فعال' ?>)
                 </span>
                 <span class="tool-c">
                     <svg xmlns="http://www.w3.org/2000/svg"
@@ -84,9 +84,7 @@ include_once('resources/views/scripts/activeNotActive.php');
                         <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3l58.3 0c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24l0-13.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1l-58.3 0c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
                     </svg>
                     <div class="tool-t">
-                        این قسمت برای مطب و کلینیک‌هایی است که تنها یک داکتر دارن یا میخواهند نسخه جدید در همان اتاق داکتر چاپ شود.
-                        توجه نمائید که با فعال کردن این گزینه نیاز است که هر داکتر یک پرینتر داشته باشد.
-                        <div class="tool-arrow"></div>
+                        با غیرفعال کردن این قسمت، بخش زمان مصرف دارو نمایش داده نمی شود.
                     </div>
                 </span>
             </span>
@@ -94,13 +92,13 @@ include_once('resources/views/scripts/activeNotActive.php');
                 <input
                     type="checkbox"
                     class="setting-toggle"
-                    data-url="change-status-pre-print"
-                    data-target="#sellStatus"
+                    data-url="change-status-intake-time"
+                    data-target="#intake_time"
                     data-true-text="(فعال)"
                     data-false-text="(غیر فعال)"
                     data-true-class="color-green"
                     data-false-class="color-orange"
-                    <?= ($settings['single_print'] == 1) ? 'checked' : '' ?>>
+                    <?= ($settings['intake_time'] == 1) ? 'checked' : '' ?>>
                 <span class="m-slider"></span>
             </label>
         </div>
