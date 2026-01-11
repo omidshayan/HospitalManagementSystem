d0016a0884639dc506d8065f08c95978ef6cd9f0
omidshayan <omidshayan.dev@gmail.com>
2026-01-11 22:59:45 +0430

    add MY_NAME

--- config.php
+++ config.php
@@ -19,6 +19,7 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'hms_sis');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
+define('MY_NAME', 'sehhatHerat');
 
 define('CPU', 'BFEBFBFF000306C3');
 define('HDD', 'S250NXAH334410L');
